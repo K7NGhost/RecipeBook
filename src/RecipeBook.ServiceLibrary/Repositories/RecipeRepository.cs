e33549af6a867199ec1c62124ecd35ed069c2493
K7NGhost <Kevinargueta05@gmail.com>
2025-09-01 22:36:33 -0400

    Add project files.

---
+++ src/RecipeBook.ServiceLibrary/Repositories/RecipeRepository.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using RecipeBook.ServiceLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeBook.ServiceLibrary.Repositories
+{
+    public interface IRecipeRepository
+    {
+        Task<int> InsertAsync(RecipeEntity entity);
+    }
+    public class RecipeRepository : IRecipeRepository
+    {
+        public async Task<int> InsertAsync(RecipeEntity entity)
+        {
+            using (var connection = new SqlConnection("Data Source=host.docker.internal,5050;Initial Catalog=RecipeBook;User Id=sa;Password=P@ssword123;TrustServerCertificate=True"))
+            {
+                var rowsAffected = await connection.ExecuteAsync(@"
+                INSERT INTO [dbo].[Recipes]
+                           ([Id]
+                           ,[Title]
+                           ,[Description]
+                           ,[Logo]
+                           ,[CreatedDate])
+                     VALUES
+                           (@Id
+                           ,@Title
+                           ,@Description
+                           ,@Logo
+                           ,@CreatedDate)",
+                   new
+                   {
+                       entity.Id,
+                       entity.Title,
+                       entity.Description,
+                       entity.Logo,
+                       entity.CreatedDate
+                   });
+                return rowsAffected;
+            }
+        }
+    }
+}
---
+++ tests/RecipeBook.ServiceLibrary.Tests/Repositories/RecipeRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using RecipeBook.ServiceLibrary.Entities;
+using RecipeBook.ServiceLibrary.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeBook.ServiceLibrary.Tests.Repositories
+{
+    public class RecipeRepositoryTests
+    {
+        [Fact]
+        public async Task InsertAsync_Success()
+        {
+            var recipeRepository = new RecipeRepository();
+            var rowsAffected = await recipeRepository.InsertAsync(new RecipeEntity()
+            {
+                Id = Guid.NewGuid(),
+                Title = "Fried Chicken Unit Test",
+                Description = "Fired Chicken Descirption",
+                Logo = null,
+                CreatedDate = DateTimeOffset.UtcNow
+            });
+
+            Assert.Equal(1, rowsAffected);
+        }
+    }
+}
